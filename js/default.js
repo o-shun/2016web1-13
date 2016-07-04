a9e8b0527cab7215c21e1bc70d6e6011dacd0293
o-shun <zyaooo99@yahoo.co.jp>
2016-07-04 17:47:23 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,12 +8,16 @@ var separate_time = function(time){
   return [sec, min, hours, days, month, year];
 }
 
-var now = new Date();
-var counter = separate_time(now);
-document.getElementById('countdown').textContent =
-  counter[5] + '年' +
-  counter[4] + '月' +
-  counter[3] + '日' +
-  counter[2] + '時' +
-  counter[1] + '分' +
-  counter[0] + '秒' ;
+var update = function(){
+  var now = new Date();
+  var target = new Date(2020,7,24,0,0,0,0);
+  var counter = separate_time(diff);
+  document.getElementById('countdown').textContent =
+    counter[5] + '年' +
+    counter[4] + '月' +
+    counter[3] + '日' +
+    counter[2] + '時' +
+    counter[1] + '分' +
+    counter[0] + '秒' ;
+  refresh();
+}
