1034ab2c3acd5444f83c394a59910255ed027fa2
o-shun <zyaooo99@yahoo.co.jp>
2016-07-04 17:52:53 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -11,6 +11,7 @@ var separate_time = function(time){
 var update = function(){
   var now = new Date();
   var target = new Date(2020,7,24,0,0,0,0);
+  var diff = target.getTime() - now.getTime();
   var counter = separate_time(diff);
   document.getElementById('countdown').textContent =
     counter[5] + '年' +
